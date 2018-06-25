8f26e8c18ec027b838be3ccec1f3797a726733db
tobirick <tobirickmann@gmail.com>
2018-06-25 15:06:04 +0200

    add guestroute middleware

--- App/routes.php
+++ App/routes.php
@@ -3,16 +3,16 @@
 use \Core\Middleware;
 use \Core\BaseController;
 
-$router->map('GET', '/login', 'AuthController@loginIndex', 'login.index');
-$router->map('POST', '/login', 'AuthController@login', 'login');
-$router->map('GET', '/logout', Middleware::requireLogin('AuthController@logout'), 'logout');
-$router->map('GET', '/register', 'AuthController@registerIndex', 'register.index');
-$router->map('POST', '/register', 'AuthController@register', 'register');
+$router->map('GET', '/login', Middleware::guestRoute('AuthController@loginIndex'), 'login.index');
+$router->map('POST', '/login', Middleware::guestRoute('AuthController@login'), 'login');
+$router->map('GET', '/logout', Middleware::userRoute('AuthController@logout'), 'logout');
+$router->map('GET', '/register', Middleware::guestRoute('AuthController@registerIndex'), 'register.index');
+$router->map('POST', '/register', Middleware::guestRoute('AuthController@register'), 'register');
 
-$router->map('GET', '/[i:id]', Middleware::requireLogin('IndexController@test'), 'testroute');
-$router->map('GET', '/', Middleware::requireLogin('IndexController@index'), 'index');
+$router->map('GET', '/[i:id]', Middleware::userRoute('IndexController@test'), 'testroute');
+$router->map('GET', '/', Middleware::userRoute('IndexController@index'), 'index');
 
-$router->map('GET', '/create', Middleware::requireLogin('IndexController@create'));
+$router->map('GET', '/create', Middleware::userRoute('IndexController@create'));
 
 $router->map('GET', '/test/[i:id]/[a:text]', function($params) {
     BaseController::view('test', $params)->render();
--- Core/Middleware.php
+++ Core/Middleware.php
@@ -3,10 +3,17 @@
 namespace Core;
 
 class Middleware {
-    public static function requireLogin($ctrl) {
+    public static function userRoute($ctrl) {
         if(isset($_SESSION['userid'])) {
             return $ctrl;
         }
         return 'AuthController@loginIndex';
+    }
+
+    public static function guestRoute($ctrl) {
+        if(isset($_SESSION['userid'])) {
+            return 'IndexController@index';
+        }
+        return $ctrl;
     }
 }
